5ebdedd7cee810f31791b840e1e113ae099e5a81
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2023-03-25 23:54:45 +0530

    ping_pong_game

--- Ping_pong_game/script.js
+++ Ping_pong_game/script.js
@@ -12,29 +12,77 @@ ball.style.marginTop='262px'
 let W_pressed=false
 let S_pressed=false
 
+let ID
+
+let Vx=-1
+let Vy=-1
+let V=Math.sqrt(Math.sqrt(Math.pow(Vx,2)+Math.pow(Vy,2)))
 document.addEventListener('keydown',(e)=>{
-    if(e.KeyCode=='87'){
+    if(e.key=='w'){
         W_pressed= true
     }
-    else if(e.KeyCode=='83'){
+    else if(e.key=='s'){
         S_pressed=true
     }
 })
 
 document.addEventListener('keyup',(e)=>{
-    if(e.KeyCode=='87'){
+    if(e.key=='w'){
         W_pressed= false
     }
-    else if(e.KeyCode=='83'){
+    else if(e.key=='s'){
         S_pressed=false
     }
 })
 
 gameLoop()
 
+function reset(){
+    clearInterval(ID)
+    Vx=-1
+    Vy=-1
+    V=Math.sqrt(Math.sqrt(Math.pow(Vx,2)+Math.pow(Vy,2)))
+    ball.style.marginLeft='534px'
+    ball.style.marginTop='262px'
+    gameLoop()
+}
+
 function gameLoop(){
     setTimeout(()=>{
-        setInterval(()=>{
+        ID=setInterval(()=>{
+            if(marginLeft(ball)<0){
+                document.querySelector('#comp-score').innerHTML=Number(document.querySelector('#comp-score').innerHTML)+1
+                reset()
+                return
+            }
+            if(marginLeft(ball)+20>1088){
+                document.querySelector('#user-score').innerHTML=Number(document.querySelector('#user-score').innerHTML)+1
+                reset()
+                return
+            }
+            if(marginTop(ball)<0||(marginTop(ball)+20)>544){
+                Vy=-Vy
+            }
+
+            let paddle=(marginLeft(ball)+10<544)?userPaddle:compPaddle
+
+            if (collisionDetected(paddle)){
+                let angle
+                let type=(marginLeft(paddle)==30)?'user':'comp'
+                if(ball.centerY<paddle.centerY){
+                    angle=(type=='user'?-Math.PI/4:(-3*Math.PI)/4)
+                }
+                else if(ball.centerY>paddle.centerY){
+                    angle=(type=='user'?Math.PI/4:(3*Math.PI)/4)
+                }
+                else if(ball.centerY==paddle.centerY){
+                    angle=(type=='user'?0:Math.PI)
+                }
+            }
+
+            ball.style.marginLeft=`${marginLeft(ball)+Vx}px`
+            ball.style.marginTop=`${marginTop(ball)+Vy}px`
+
             if(W_pressed && marginTop(userPaddle)>0){
                 userPaddle.style.marginTop=`${marginTop(userPaddle)-2}px`
             }
@@ -44,6 +92,25 @@ function gameLoop(){
         },5)
     },500)
 }
+
+function collisionDetected(paddle){
+    ball.top=marginTop(ball)
+    ball.bottom=marginTop(ball)+20
+    ball.left=marginTop(ball)
+    ball.right=marginTop(ball)+20
+    ball.centerX=marginTop(ball)+10
+    ball.centerY=marginTop(ball)+20
+
+    paddle.top=marginTop(paddle)
+    paddle.bottom=marginTop(paddle)+72
+    paddle.left=marginTop(paddle)
+    paddle.right=marginTop(paddle)+10
+    paddle.centerX=marginTop(paddle)+5
+    paddle.centerY=marginTop(paddle)+36
+
+    return ball.left<paddle.right&&ball.top<paddle.bottom&&ball.right>paddle.left&&ball.bottom>paddle.top
+}
+
 
 function marginTop(elem){
     return Number(elem.style.marginTop.split('p')[0])
