5aa4e1c49794c8c6edb0694ff172d57864e2855c
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2023-03-31 23:55:07 +0530

    Create Q2_reverse_arr.java

---
+++ prac_ass_3/Q2_reverse_arr.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        System.out.print("arrNum:");
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+        System.out.print("The elements in reverse order - ");
+        for(int i=n-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+        s.close();
+    }
+} 
