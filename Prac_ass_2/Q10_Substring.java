dde78d3d6cb3e04595d2b353afbbcb088c0a03aa
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2023-04-02 15:36:38 +0530

    cond_loop

--- Prac_ass_2/Q10_Substring.java
+++ Prac_ass_2/Q10_Substring.java
@@ -3,6 +3,7 @@ public class Main
 {
 	public static void main(String[] args) {
 	    Scanner s=new Scanner(System.in);
+	    try{	
 		System.out.print("String Name:");
 		String str=s.next();
 		System.out.print("Starting index:");
@@ -10,5 +11,9 @@ public class Main
 		System.out.print("Ending index:");
 		int end=s.nextInt();
 		System.out.println("subString of "+str+ "from "+start+" to " +end+ " is: "+str.substring(start,end+1));
+	    }
+            catch(Exception e){
+		    System.out.println("Invalid");
+	    }		    
 	}
 }
